ffcf1fe2db3750e860effa799500118b9e132888
digvijay <digvijay01210@gmail.com>
2025-11-13 11:21:39 +0530

    push for deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,8 @@ ALLOWED_HOSTS = [
     'citypulse-production-73fb.up.railway.app' # <-- Allows the public domain
 ]
 
+DEBUG = False
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
